f571a2d1848a685e9a5469e0db0c0a143c074098
Laochra <blue.cat.blue@outlook.com>
2023-07-20 16:46:47 +1000

    Refactored Tower Creation script
    Raycasts are now only performed when needed as opposed to every frame.

--- Assets/Game/Pen/Laochra/TowerCreation.cs
+++ Assets/Game/Pen/Laochra/TowerCreation.cs
@@ -1,9 +1,16 @@
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public enum InteractionState
+{
+    None,
+    DraggingBud,
+    PositionSelected
+}
+
 public class TowerCreation : MonoBehaviour
 {
+    [SerializeField] private bool debugMode;
     [SerializeField] private KeyCode interactKey = KeyCode.Mouse0;
 
     private Vector3 mouseScreenPosition;
@@ -16,25 +23,66 @@ public class TowerCreation : MonoBehaviour
     [SerializeField] private float placementExclusionSize = 5;
     private const float capsuleCheckBound = 5;
 
+    private InteractionState currentInteraction = InteractionState.None;
+    private RaycastHit currentHit;
+
     private void Update()
     {
         mouseScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.1f);
         mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
 
-        RaycastHit hit = GetRayHit();
-
-        if (hit.collider is null)
+        switch(currentInteraction)
         {
-            Debug.DrawRay(Camera.main.transform.position, (mouseWorldPosition - Camera.main.transform.position) * 1000, Color.red);
+            case InteractionState.None:
+                DraggingBudState();
+                break;
+            case InteractionState.DraggingBud:
+                DraggingBudState();
+                break;
+            case InteractionState.PositionSelected:
+                PositionSelectedState();
+                break;
+        }
+
+        if(debugMode)
+        {
+            if (GetRayHit().collider is null)
+                Debug.DrawRay(Camera.main.transform.position, (mouseWorldPosition - Camera.main.transform.position) * 1000, Color.red);
+            else
+                Debug.DrawRay(Camera.main.transform.position, (mouseWorldPosition - Camera.main.transform.position).normalized * GetRayHit().distance, Color.green);
+        }
+    }
+
+
+
+    private void DraggingBudState()
+    {
+        if (Input.GetKeyUp(interactKey))
+        {
+            currentHit = GetRayHit();
+
+            if (currentHit.collider is not null)
+                currentInteraction = InteractionState.PositionSelected;
+            else
+                currentInteraction = InteractionState.None;
+        }
+    }
+
+    private void PositionSelectedState()
+    {
+        if (!TargetIsPlane())
+        {
+            currentInteraction = InteractionState.None;
             return;
         }
-        Debug.DrawRay(Camera.main.transform.position, (mouseWorldPosition - Camera.main.transform.position).normalized * hit.distance, Color.green);
 
-        bool targetIsPlane = CheckForPlane(hit);
+        if (SpaceForTower())
+            SpawnTower();
 
-        if (targetIsPlane && Input.GetKeyDown(interactKey))
-            AttemptToSpawnTower(hit);
+        currentInteraction = InteractionState.None;
     }
+
+
 
     private RaycastHit GetRayHit()
     {
@@ -42,19 +90,24 @@ public class TowerCreation : MonoBehaviour
         return hit;
     }
 
-    private bool CheckForPlane(RaycastHit hit)
+    private bool TargetIsPlane()
     {
-        return hit.collider.gameObject == targetPlane;
+        return currentHit.collider.gameObject == targetPlane;
     }
 
-    private void AttemptToSpawnTower(RaycastHit hit)
+    private bool SpaceForTower()
     {
-        Vector3 capsuleTop = new(hit.point.x, hit.point.y + capsuleCheckBound, hit.point.z);
-        Vector3 capsuleBottom = new(hit.point.x, hit.point.y - capsuleCheckBound, hit.point.z);
+        Vector3 capsuleTop = new(currentHit.point.x, currentHit.point.y + capsuleCheckBound, currentHit.point.z);
+        Vector3 capsuleBottom = new(currentHit.point.x, currentHit.point.y - capsuleCheckBound, currentHit.point.z);
 
         var targets = Physics.CapsuleCastAll(capsuleTop, capsuleBottom, placementExclusionSize, Vector3.up, Mathf.Infinity, layersToCheck).ToList();
+        
         Debug.Log(targets.Count);
-        if (targets.Count == 0)
-            Instantiate(towerPrefab, hit.point, Quaternion.identity);
+        return targets.Count == 0; 
+    }
+
+    private void SpawnTower()
+    {
+        Instantiate(towerPrefab, currentHit.point, Quaternion.identity);
     }
 }
