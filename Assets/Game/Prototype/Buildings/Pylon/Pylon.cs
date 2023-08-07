2ac8926ed2874c899482cf28f5b698bc4d2f5990
Lochlan.R <lrobichaud666@gmail.com>
2023-08-07 10:56:12 +1000

    merging remaining files

--- Assets/Game/Prototype/Buildings/Pylon/Pylon.cs
+++ Assets/Game/Prototype/Buildings/Pylon/Pylon.cs
@@ -1,28 +1,59 @@
+<<<<<<< HEAD
 using System.Collections.Generic;
 using UnityEngine;
+=======
+>>>>>>> bd1fdbe35e0e26cdee5d69b7a12991881e13b72c
 using BuildingList = System.Collections.Generic.List<Building>;
+using ContextMenu = UnityEngine.ContextMenu;
+
+using Debug = UnityEngine.Debug;
 
 public class Pylon : Building
 {
+<<<<<<< HEAD
     private BuildingList connectedBuildings = new();
     private List<Tower> connectedTowerList = new();
     private int buildingCount;
 
     public int PylonEXP;
+=======
+    public bool Enhanced { get; private set; }
+    private readonly BuildingList connectedBuildings = new();
+    public bool IsBuildingInList(Building building)
+    {
+        return connectedBuildings.Contains(building);
+    }
+>>>>>>> bd1fdbe35e0e26cdee5d69b7a12991881e13b72c
 
+    private void Update()
+    {
+        for (int buildingIndex = 0; buildingIndex < connectedBuildings.Count; buildingIndex++)
+        {
+            Building building = connectedBuildings[buildingIndex];
+            if (building is Tower)
+            {
+                if ((building as Tower).sellFlag)
+                    SellTower(building as Tower);
+            }
+        }
+    }
+
+
+    public void Enhance() => Enhanced = true;
     public void AddBuilding(Building building) => connectedBuildings.Add(building);
 
-    [ContextMenu("Deactivate")]
-    public override void Deactivate()
+    [ContextMenu("Deactivate")] public override void Deactivate()
     {
-        base.Deactivate();
+        DeactivateConnectedBuildings();
 
+        base.Deactivate();
+    }
+    public void DeactivateConnectedBuildings()
+    {
         foreach (Building building in connectedBuildings)
         {
             building.Deactivate();
         }
-
-        Debug.Log("Deactivated");
     }
     public override void Reactivate()
     {
@@ -34,6 +65,7 @@ public class Pylon : Building
         }
     }
 
+<<<<<<< HEAD
     private void Update()
     {
         // Move this to on wave end in the wave manager when it exists.
@@ -71,3 +103,40 @@ public class Pylon : Building
 
 // sorry if my lack of knowledge regarding Context menus, regions and serialize field throw you off.
 // I'll make sure to comment on what is my code. _Lochlan_
+=======
+    public override void Sell()
+    {
+        DeactivateConnectedBuildings();
+
+        base.Sell();
+    }
+    public void SellTower(Tower tower)
+    {
+        Debug.Log("Sold Tower", tower);
+        connectedBuildings.Remove(tower);
+        Destroy(tower.gameObject);
+    }
+    public void SellAll()
+    {
+        BuildingList sellList = new();
+
+        for (int buildingIndex = 0; buildingIndex < connectedBuildings.Count; buildingIndex++)
+        {
+            Building building = connectedBuildings[buildingIndex];
+            if (building is Pylon)
+                (building as Pylon).SellAll();
+            else
+            {
+                sellList.Add(building);
+            }
+        }
+
+        for (int buildingIndex = 0; buildingIndex < sellList.Count; buildingIndex++)
+        {
+            SellTower(sellList[buildingIndex] as Tower);
+        }
+
+        base.Sell();
+    }
+}
+>>>>>>> bd1fdbe35e0e26cdee5d69b7a12991881e13b72c
--- Assets/Game/Prototype/Buildings/Towers/Tower.cs
+++ Assets/Game/Prototype/Buildings/Towers/Tower.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
@@ -16,4 +17,36 @@ public class Tower : Building
        EditorApplication.Beep(); // Why Connor?
     }  
 #endif
+=======
+using Debug = UnityEngine.Debug;
+using GameObjectList = System.Collections.Generic.List<UnityEngine.GameObject>;
+using UnityEngine;
+public class Tower : Building
+{
+    [SerializeField] bool upgraded;
+    public bool Upgraded { get; private set; }
+    public bool sellFlag;
+
+    [SerializeField] GameObjectList upgradedTowerPrefabs;
+
+    public void Upgrade(int upgradePath)
+    {
+        if (upgradePath >= 0 && upgradePath < upgradedTowerPrefabs.Count)
+        {
+            Transform transform = gameObject.transform;
+            Instantiate(upgradedTowerPrefabs[upgradePath], transform.position, transform.rotation, transform.parent);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.LogError("Upgrade only accepts an int value between 0 and its amount of possible upgrades (" + upgradedTowerPrefabs.Count + ").", this);
+        }
+    }
+
+    public override void Sell()
+    {
+        sellFlag = true;
+        Destroy(gameObject, 0.1f);
+    }
+>>>>>>> bd1fdbe35e0e26cdee5d69b7a12991881e13b72c
 }
