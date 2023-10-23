bc2c2ef485786f636299bb80e2be44c151167a7c
XxdalendxX <connordr112@gmail.com>
2023-10-23 16:53:10 +1100

    started working on a non prototype tutorial prefab

--- Assets/Game/Production/Utility/UI/Prototype Instructions/InstructionUI.cs
+++ Assets/Game/Production/Utility/UI/Prototype Instructions/InstructionUI.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[System.Serializable]
+struct Page
+{
+    public Image instructionPanel;
+    [TextArea] public  string instructionText;
+}
+
 public class InstructionUI : MonoBehaviour
 {
+    [SerializeField] Page[] instructionPages;
 
     [SerializeField] Button openInstButton;
     [SerializeField] GameObject instructionPanel;
+
+    //private variables
+    int pageNumber = 0;
 
     private void Start()
     {
@@ -26,5 +37,6 @@ public class InstructionUI : MonoBehaviour
     {
         instructionPanel.SetActive(false);
         openInstButton.interactable = true;
+        pageNumber = 0;
     }
 }
